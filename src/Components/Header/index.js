72c5c496383c446fc72036bc8436f46aba8ab67e
unknown <tusha@nowpurchase.com>
2024-02-01 16:15:20 +0530

    adding drawer on header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,8 +6,8 @@ import Logo from "../../assets/images/logo.png";
 import ArrowDown from "../../assets/images/icons/arrowDown.png";
 import VectorIcon from "../../assets/images/vector.png";
 import ServicesMenu from "../Home/ServicesMenu";
- import menuPic from "../../assets/images/home/menuItem1.png"
-import menuPic2 from "../../assets/images/home/menuItem2.png"
+import menuPic from "../../assets/images/home/menuItem1.png";
+import menuPic2 from "../../assets/images/home/menuItem2.png";
 // import crossBtn from "../../assets/images/crossBtnf.png";
 // import submitBtn from "../../assets/images/submit.png";
 
@@ -31,11 +31,11 @@ function Header() {
   const headerTitle = headerTitleJSON.find(
     ({ url }) => url === window.location.pathname
   );
- useEffect(() => {
-   if (headerTitle?.text) {
-     document.title = "NowPurchase || "+ headerTitle.text;
-   }
- }, [headerTitle]);
+  useEffect(() => {
+    if (headerTitle?.text) {
+      document.title = "NowPurchase || " + headerTitle.text;
+    }
+  }, [headerTitle]);
   const [isOpen, setIsOpen] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
   function openMenu() {
@@ -51,7 +51,15 @@ function Header() {
     "Requeest Demo",
     "Others",
   ].map((items) => ({ label: items, value: items }));
- //console.log(optionsData);
+  //console.log(optionsData);
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleDrawer = () => {
+    setIsVisible(!isVisible);
+    console.log(isVisible);
+  };
+  console.log(modalOpen,'kkk')
   return (
     <>
       <Modal open={modalOpen} onClose={handleClose}>
@@ -203,33 +211,115 @@ function Header() {
                 </div>
               </div>
             </div> */}
-            <div class="btnWrapNew mobBtnWrapNew">
-              <ul>
-                <li>
-                  <a href="#getInTouchForm" class="getInTouch">
-                    Get in touch
-                  </a>
-                </li>
-                <li class="mobWrapBtn">
-                  <a
-                    class="mobNavBtn mobNavBtnCrossBar"
-                    href="javascript:void(0);"
-                  >
-                    <span class="bar bar1"></span>
-                    <span class="bar bar2"></span>
-                    <span class="bar bar3"></span>
-                  </a>
-                  <a
-                    class="mobNavBtn mobNavBtnCross"
-                    href="javascript:void(0);"
-                  >
-                    <span class="bar bar1"></span>
-                    <span class="bar bar2"></span>
-                    <span class="bar bar3"></span>
-                  </a>
-                </li>
-              </ul>
-            </div>
+         
+              <div class="btnWrapNew mobBtnWrapNew">
+                <ul>
+                  <li onClick={() => setModalOpen(true)}>
+                    <a href="#getInTouchForm" class="getInTouch">
+                      Get in touch
+                    </a>
+                  </li>
+                  <li class="mobWrapBtn" onClick={toggleDrawer}>
+                    <a
+                      className={`mobNavBtn mobNavBtnCrossBar ${
+                        isVisible ? "visible" : "hidden"
+                      }`}
+                      href="javascript:void(0);"
+                    >
+                      <span class="bar bar1"></span>
+                      <span class="bar bar2"></span>
+                      <span class="bar bar3"></span>
+                    </a>
+                    <a
+                      class={`mobNavBtn mobNavBtnCross ${
+                        isVisible ? "hidden" : "visible"
+                      }`}
+                      href="javascript:void(0);"
+                    >
+                      <span class="bar bar1"></span>
+                      <span class="bar bar2"></span>
+                      <span class="bar bar3"></span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              {!isVisible && (
+                <div class="col-lg-7 navigationWrapNew slide">
+                <div class="row">
+                  <div class="col-lg-7 navWrapNew">
+                    <nav>
+                      <ul>
+                        <li>
+                          <a href="#" id="Header_Services">Services
+                            <span class="toggleBtn"><img loading="lazy" src={ArrowDown} alt=""/></span></a>
+                          <div class="megaMenuNew">
+                            <div class="container">
+                            
+                              <div class="megaMenuWrap">
+                                <ul class="noList">
+                                  <li>
+                                    <figure>
+                                      <img loading="lazy" src={menuPic} alt=""/>
+                                      <figcaption>MarketPlace</figcaption>
+                                    </figure>
+                                    <div class="figCont">
+                                      <p>
+                                        <strong>One-stop</strong> Solution <br/>for
+                                        Quality Procurement
+                                      </p>
+                                      <a href="marketplace.html" id="Header_Services_Marketplace" class="knowMoreBtn gradientBtn">Know More</a>
+                                    </div>
+                                  </li>
+                                  <li>
+                                    <figure>
+                                      <img loading="lazy" src={menuPic2} alt=""/>
+                                      <figcaption>MetalCloud</figcaption>
+                                    </figure>
+                                    <div class="figCont">
+                                      <p>
+                                        Your Melting Process, <br/>Now Made
+                                        <strong>Reliable!</strong>
+                                      </p>
+                                      <a href="application.html" class="knowMoreBtn gradientBtn" id="Header_Services_MetalCloud">Know More</a>
+                                    </div>
+                                  </li>
+                                </ul>
+                              </div>
+                  
+                            </div>
+                          </div>
+                        </li>
+                        <li>
+                          <a href="culture.html" id="Header_Cluture">Culture</a>
+                        </li>
+                        <li>
+                          <a href="career.html" id="Header_Careers">Careers</a>
+                        </li>
+                       
+                      </ul>
+                    </nav>
+                  </div>
+                  <div class="col-lg-5 btnWrapNew">
+                    <ul>
+                      <li>
+                        <a href="/getInTouchForm" class="getInTouch" id="Header_GetInTouch">Get in touch</a>
+                      </li>
+                      <li>
+                        <a href="https://app.nowpurchase.com/login" target="_blank" class="gotoApp" id="Header_GoToMetalCloud">
+                          <span class="gotoTopPart">
+                            <span>Go to</span>
+                            <img loading="lazy" src={VectorIcon} alt=""/>
+                          </span>
+                          NP MetalCloud
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              )}
+           
             <div className="col-lg-7 navigationWrapNew">
               <div className="row">
                 <div className="col-lg-7 navWrapNew">
@@ -265,10 +355,8 @@ function Header() {
                 </div>
                 <div class="col-lg-5 btnWrapNew">
                   <ul>
-                    <li>
-                      <a onClick={() => setModalOpen(true)} class="getInTouch">
-                        Get in touch
-                      </a>
+                    <li onClick={() => setModalOpen(true)}>
+                      <a class="getInTouch">Get in touch</a>
                     </li>
                     <li>
                       <a
