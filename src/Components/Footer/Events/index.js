69dc12ef5d7ed7ff4e513116e5c1fb4021ae8049
unknown <tusha@nowpurchase.com>
2024-02-15 13:07:12 +0530

    events page modified

--- src/Components/Footer/Events/index.js
+++ src/Components/Footer/Events/index.js
@@ -6,108 +6,13 @@ import eventIFEX5 from "../../../assets/images/events/eventIFEX5.png";
 import eventIFEX3 from "../../../assets/images/events/eventIFEX3.png";
 import eventIFEX4 from "../../../assets/images/events/eventIFEX4.png";
 import eventIFEX2 from "../../../assets/images/events/eventIFEX2.png";
+import eventIfex1 from "../../../assets/images/events/IFEX 2024.jpeg"
+import eascon from "../../../assets/images/events/EASCON 2024.png";
 import eventBg from "../../../assets/images/events/eventBg.png";
 function EventsPage() {
   return (
     <>
       <div className="event1">
-        <div className="container">
-          <div className="eventHead aosAnim">
-            <h3 className="titleText">Upcoming Events</h3>
-            <p>
-              Listed are events that NowPurchase is going to be a part of. These
-              are tentative and <br />
-              can change at any time without prior notice.
-            </p>
-          </div>
-          <div className="eventMWrap aosAnim">
-            <div className="tabContBox">
-              <div className="topBar">
-                <h4>INTER FOUNDRY 2024</h4>
-                <div className="listBox">
-                  <ul>
-                    <li>
-                      <img loading="lazy" src={mapIcon} alt="" />
-                      <span>Codissia Trade Fair Complex, Coimbatore</span>
-                    </li>
-                    <li>
-                      <img loading="lazy" src={timerIcon} alt="" />
-                      <span>August 22 - 24, 2024</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="mainBox">
-                <a
-                  href="http://www.fdfindia.org/"
-                  target="_blank"
-                  className="knowMoreBtn gradientBtn"
-                >
-                  View Details
-                </a>
-              </div>
-            </div>
-            <div className="tabContBox">
-              <div className="topBar">
-                <h4>IFFE 2024</h4>
-                <div className="listBox">
-                  <ul>
-                    <li>
-                      <img loading="lazy" src={mapIcon} alt="" />
-                      <span>Kolhapur, India</span>
-                    </li>
-                    <li>
-                      <img loading="lazy" src={timerIcon} alt="" />
-                      <span>Febr/uary (TBD)</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="mainBox"></div>
-            </div>
-            <div className="tabContBox">
-              <div className="topBar">
-                <h4>IFEX 2024</h4>
-                <div className="listBox">
-                  <ul>
-                    <li>
-                      <img loading="lazy" src={mapIcon} alt="" />
-                      <span>
-                        Bangalore International Exhibition Centre, Bengaluru,
-                        India
-                      </span>
-                    </li>
-                    <li>
-                      <img loading="lazy" src={timerIcon} alt="" />
-                      <span>2-4th Febr/uary, 2024</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="mainBox"></div>
-            </div>
-            <div className="tabContBox">
-              <div className="topBar">
-                <h4>Cast Indo Expo</h4>
-                <div className="listBox">
-                  <ul>
-                    <li>
-                      <img loading="lazy" src={mapIcon} alt="" />
-                      <span>BIEC, Bangalore</span>
-                    </li>
-                    <li>
-                      <img loading="lazy" src={timerIcon} alt="" />
-                      <span>2-4th Febr/uary, 2024</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="mainBox"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <section class="event1 event2 pt-0">
         <div class="container">
           <div class="eventHead aosAnim">
             <h3 class="titleText">Previous Events</h3>
@@ -116,7 +21,84 @@ function EventsPage() {
               are tentative and can change at any time without prior notice.
             </p>
           </div>
-
+          <div class="eventFull aosAnim">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="eventImgM">
+                  <img
+                    loading="lazy"
+                    style={{
+                      borderTopLeftRadius: "20px",
+                      borderBottomLeftRadius: "20px",
+                    }}
+                    src={eventIfex1}
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="tabContBox">
+                  <div class="topBar">
+                    <h4>IFEX 2024 – 20th International Foundry Exhibition</h4>
+                  </div>
+                  <div class="mainBox">
+                    <div class="listBox">
+                      <ul>
+                        <li>
+                          <img loading="lazy" src={mapIcon} alt="" />
+                          <span>
+                            Bangalore International Exhibition Centre (BIEC)
+                          </span>
+                        </li>
+                        <li>
+                          <img loading="lazy" src={timerIcon} alt="" />
+                          <span>February 2nd - 4th, 2024</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="eventFull aosAnim">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="eventImgM">
+                  <img
+                    loading="lazy"
+                    style={{
+                      borderTopLeftRadius: "20px",
+                      borderBottomLeftRadius: "20px",
+                    }}
+                    src={eascon}
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="tabContBox">
+                  <div class="topBar">
+                    <h4>EASCON - Re CASTING East</h4>
+                  </div>
+                  <div class="mainBox">
+                    <div class="listBox">
+                      <ul>
+                        <li>
+                          <img loading="lazy" src={mapIcon} alt="" />
+                          <span>Park Prime Hotel, Kolkata</span>
+                        </li>
+                        <li>
+                          <img loading="lazy" src={timerIcon} alt="" />
+                          <span>January 13, 2024</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="eventMWrap aosAnim">
             <div class="tabContBox">
               <div class="topBar">
@@ -395,6 +377,64 @@ function EventsPage() {
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="event1 event2 pt-0">
+        <div className="container">
+          <div className="eventHead aosAnim">
+            <h3 className="titleText">Upcoming Events</h3>
+            <p>
+              Listed are events that NowPurchase is going to be a part of. These
+              are tentative and <br />
+              can change at any time without prior notice.
+            </p>
+          </div>
+          <div className="eventMWrap aosAnim">
+            <div className="tabContBox">
+              <div className="topBar">
+                <h4>INTER FOUNDRY 2024</h4>
+                <div className="listBox">
+                  <ul>
+                    <li>
+                      <img loading="lazy" src={mapIcon} alt="" />
+                      <span>Codissia Trade Fair Complex, Coimbatore</span>
+                    </li>
+                    <li>
+                      <img loading="lazy" src={timerIcon} alt="" />
+                      <span>August 22 - 24, 2024</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="mainBox">
+                <a
+                  href="http://www.fdfindia.org/"
+                  target="_blank"
+                  className="knowMoreBtn gradientBtn"
+                >
+                  View Details
+                </a>
+              </div>
+            </div>
+            <div className="tabContBox">
+              <div className="topBar">
+                <h4>IFFE 2024</h4>
+                <div className="listBox">
+                  <ul>
+                    <li>
+                      <img loading="lazy" src={mapIcon} alt="" />
+                      <span>Kolhapur, India</span>
+                    </li>
+                    <li>
+                      <img loading="lazy" src={timerIcon} alt="" />
+                      <span>Febr/uary (TBD)</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="mainBox"></div>
             </div>
           </div>
         </div>
